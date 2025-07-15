50bb1559ad879e2c88612ccf3eb51b0e7f034206
Sara Tapusoa <79812543+stapusoa@users.noreply.github.com>
2025-07-14 20:43:44 -0600

    fix octokit

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
 export const REPO_OWNER = "stapusoa";
-export const REPO_NAME = "plantingrootsrealty-cms";
+export const REPO_NAME = "plantingrootsrealty";
 export const CONTENT_PATH = "content/pages";
 export const BRANCH = "home"; // or 'main'
