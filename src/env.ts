c3a9ab698a20d686e68da190e6ad708554bced1d
Sara Tapusoa <79812543+stapusoa@users.noreply.github.com>
2025-07-14 21:06:52 -0600

    fix env branch variable

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
 export const REPO_OWNER = "stapusoa";
 export const REPO_NAME = "plantingrootsrealty";
 export const CONTENT_PATH = "content/pages";
-export const BRANCH = "home"; // or 'main'
+export const BRANCH = "main";
