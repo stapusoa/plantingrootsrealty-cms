1652054288fa2e338df3b21f767c5c9ee733c599
Sara Tapusoa <79812543+stapusoa@users.noreply.github.com>
2025-07-14 21:11:52 -0600

    fix path of content path in env

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
 export const REPO_OWNER = "stapusoa";
 export const REPO_NAME = "plantingrootsrealty";
-export const CONTENT_PATH = "content/pages";
+export const CONTENT_PATH = "content";
 export const BRANCH = "main";
